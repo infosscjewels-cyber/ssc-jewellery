06090b6ba99d93b08d3e16c79f5fdd9ca8a54624
Natarajan Raju <natarajan.raju1995@gmail.com>
2026-05-27 01:23:06 +0530

    Txndate bug fixed

--- server/services/iciciService.js
+++ server/services/iciciService.js
@@ -41,12 +41,16 @@ const formatTxnDate = (date = new Date()) => {
         timeZone: 'Asia/Kolkata',
         year: 'numeric',
         month: '2-digit',
-        day: '2-digit'
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
     }).formatToParts(date).reduce((acc, part) => {
         acc[part.type] = part.value;
         return acc;
     }, {});
-    return `${parts.year || ''}${parts.month || ''}${parts.day || ''}235959`;
+    return `${parts.year || ''}${parts.month || ''}${parts.day || ''}${parts.hour || ''}${parts.minute || ''}${parts.second || ''}`;
 };
 
 const parseGatewayPayload = async (response) => {
